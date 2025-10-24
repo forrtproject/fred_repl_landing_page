08f7e29c9bd2430a761feb718d6a7ffd860fe412
Nnouka Stephen <nnoukastephen@gmail.com>
2025-10-24 10:17:51 +0200

    fix: Update backend URL and correct endpoint for DOI info retrieval

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -2,11 +2,11 @@ import type { DOIAPIResponse } from "../@types";
 import { createHttp } from "../utils/http";
 
 const backend = createHttp({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://ouj1xoiypb.execute-api.eu-central-1.amazonaws.com/v1/original-lookup",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://5waa6mryb6.execute-api.eu-central-1.amazonaws.com/v1",
 });
 
 export const fetchDOIInfo = async (doi: string) => {
-  const response = await backend.get<DOIAPIResponse>('', { params: { doi } });
+  const response = await backend.get<DOIAPIResponse>('/original-lookup', { params: { doi } });
 
   return response.data;
 };
